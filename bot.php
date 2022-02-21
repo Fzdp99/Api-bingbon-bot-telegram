83ae54341e031a727148ed4f6ab7d548eaa55a09
Fatwa Zuhri <97175664+Fzdp99@users.noreply.github.com>
2022-02-21 14:12:40 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,9 +1,9 @@
 <?php
      // id bot
-     $bot = 'bot2138385957:AAF-Fhgggqn5_LAnXekrnA6w4JU6jXtc3dY';
+     $bot = 'ID_BOT';
      
      // id grub / user 
-     $user = '-729010872';
+     $user = 'ID_USER';
 
      function rendah($nm,$bot,$user,$last, $low){
           $url='https://api.telegram.org/'.$bot.'/sendMessage?chat_id='.$user.'&text=Bingbon%0aNama : '.$nm.'%0aHarga : Rendah%0aLow : '.$low.'%0aLast : '.$last.'&parse_mode=html';
